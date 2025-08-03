3fbd67e8ff84b9fa38300d176460351fce4d732e
RajasekharGanapavarapu <rajasekhar.ganapavarapu.18@gmail.com>
2025-08-03 17:53:56 +0530

    Create aboutus.js

---
+++ src/components/others/aboutus.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function AboutUs() {
+  return (
+    <div className="container py-4">
+      <h3>About Us</h3>
+      <p>
+        MovieBooker is your one-stop solution for booking movie tickets online!
+        We offer a seamless, fast, and secure way to discover, book, and enjoy movies at your favorite theatres.
+      </p>
+      <ul>
+        <li>Browse Movies by Genre and Location</li>
+        <li>Book your preferred seats and showtime</li>
+        <li>Track your bookings in your profile</li>
+        <li>Get instant support through FAQ and feedback</li>
+      </ul>
+      <p>Enjoy the show! 🍿</p>
+    </div>
+  );
+}
